dcc2ace6164877ef9711c622cd5966b8a5ee3916
Amogh <47057952+therealmog@users.noreply.github.com>
2026-08-06 01:34:33 +0100

    Working on transfering

--- calculateAmounts.py
+++ calculateAmounts.py
@@ -4,13 +4,13 @@
 import json
 import datetime
 
-START = datetime.date(2026,8,7)     # Needs to be set.
+START = datetime.date(2026,8,6)     # Needs to be set.
 
 amountsDict = {}
 date = START
 amount = 0
 for i in range(1,365,1):
-    amount = i/100
+    amount = i
     dateKey = date.strftime("%d/%m/%Y")
     dateDict = {
         "amount" : amount,
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import requests
 import datetime
+import uuid
 import json
 import os
 
@@ -11,34 +12,59 @@ if TOKEN == None:
 else:
     print("Token loaded successfully.")
 
-url = "https://api.starlingbank.com/api/v2/account"
-accountUID = "1e0f4e68-7893-46f4-ad8f-13cbe6b9ea9d" # This is the Easy Saver spaces ID.
-spacesURL = url + "/" + accountUID + "/spaces"
-transferURL = url 
+url = "https://api.starlingbank.com/api/v2/account/1e0f4e68-7893-46f4-ad8f-13cbe6b9ea9d"    # ID is the Easy Saver spaces ID.
+spacesURL = url + "/spaces"
+transferURL = url + "/savings-goals/5667db85-c91e-4a45-af58-92454ce13b29/add-money/"    # Need to add a UUID onto the end.
 
 paid = False
 amount = 0
+todaysDate = datetime.date.today().strftime("%d/%m/%Y")
+amountsDict = {}
+
+def makeTransfer(amount):
+    # Making the transfer
+    headers = {"Authorization" : f"Bearer {TOKEN}"}
+    accessURL = transferURL + str(uuid.uuid4())    # unique ID for the transfer
+
+    data = {
+        "amount" : {
+            "currency" : "GBP",
+            "minorUnits" : int(amount)
+        }
+    }
+    response = requests.put(accessURL,headers=headers, json=data)
+    print(response.status_code)
+
+    if response.status_code == 200:
+        setCompleted()
+    else:
+        print("Error")
+        print(response.headers)
+        print(response.text)
+
+def setCompleted():
+    # Sets JSON completed value to "true"
+    # Use previously accessed amountsDict
+    amountsDict[todaysDate]["completed"] = "true"
+
+    # Write the new value
+    with open("amounts.json","w") as f:
+        json.dump(amountsDict,f,indent=4)
+
 # Get the amount for today if not paid, otherwise return "paid"
 with open("amounts.json","r") as f:
-    obj = json.load(f)
-    todayDict = obj[datetime.date.today().strftime("%d/%m/%Y")]
+    amountsDict = json.load(f)
+    todayDict = amountsDict[todaysDate]
 
     if todayDict["completed"] == "true":
         print("Today's amount has already been transfered.")
     elif todayDict["completed"] == "false":
-        amount = todayDict["amount"]
+        makeTransfer(amount=todayDict["amount"])
     else:
         print("Error.")
 
 
-# Making the transfer
-headers = {"Authorization" : f"Bearer {TOKEN}"}
-response = requests.get(spacesURL,headers=headers)
-print(response.status_code)
 
-if response.status_code == 200:
-    with open("spaces.json","w") as f:
-        json.dump(response.json(),f,indent=4)
-else:
-    print(response.text)
+
+
 
